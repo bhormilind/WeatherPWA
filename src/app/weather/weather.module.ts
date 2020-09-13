aa4a5a3ed7b98865e6662e04b34c93bea4cdddc6
bhormilind <bhormilind@gmail.com>
2020-09-13 18:58:04 -0400

    Created Application Structure

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WeatherRoutingModule } from './weather-routing.module';
+import { WeatherHomeComponent } from './weather-home/weather-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WeatherHomeComponent],
   imports: [
     CommonModule,
     WeatherRoutingModule

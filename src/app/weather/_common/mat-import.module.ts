36bd6cbd74f96af50dd20cade283fd70b636d272
bhormilind <bhormilind@gmail.com>
2020-09-14 15:14:13 -0400

    Drafted UI Structure

--- src/app/weather/_common/mat-import.module.ts
+++ src/app/weather/_common/mat-import.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
-
-const matImports: any  = [
-  MatAutocompleteModule,
-  MatInputModule
-];
+const matImports: any = [MatAutocompleteModule, MatInputModule, MatCardModule];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    matImports
-  ],
-  exports: [
-    matImports
-  ]
+  imports: [CommonModule, matImports],
+  exports: [matImports],
 })
-export class MatImportModule { }
+export class MatImportModule {}
